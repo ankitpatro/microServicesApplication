cebebe6e65a10a63427b3c6c35107530c04161d3
Abhishek <kamble.a@husky.neu.edu>
2020-05-25 12:22:13 -0400

    Spring Starter Web initial Commit

---
+++ src/test/java/com/abhiani/project/microProject/MicroProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhiani.project.microProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
